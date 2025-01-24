5bc5fef7257db24463e5574915bba9300d85826c
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-01-24 13:44:50 +0100

    refactor(tennis3): :recycle: change literals for constant + fix sloppy changes
    while using the "replace all" tool from VSCODE, it had replaced the name of a variable that were defined twice (holding different data). Now the variables have been separated

--- src/tennis3.py
+++ src/tennis3.py
@@ -1,4 +1,8 @@
 class TennisGame3:
+    POINT = 1
+    MATCHPOINT = 4
+    ADVANTAGEPOINT = 6
+
     def __init__(self, leftplayer_name, rightplayer_name):
         self.leftplayer_name = leftplayer_name
         self.rightplayer_name = rightplayer_name
@@ -7,21 +11,21 @@ class TennisGame3:
 
     def won_point(self, playername):
         if playername == self.leftplayer_name:
-            self.leftplayer_points += 1
+            self.leftplayer_points += TennisGame3.POINT
         else:
-            self.rightplayer_points += 1
+            self.rightplayer_points += TennisGame3.POINT
 
     def score(self):
-        if (self.leftplayer_points < 4 and self.rightplayer_points < 4) and (self.leftplayer_points + self.rightplayer_points < 6):
+        if (self.leftplayer_points < TennisGame3.MATCHPOINT and self.rightplayer_points < TennisGame3.MATCHPOINT) and (self.leftplayer_points + self.rightplayer_points < TennisGame3.ADVANTAGEPOINT):
             score_annotation = ["Love", "Fifteen", "Thirty", "Forty"]
             current_anotatedscore = score_annotation[self.leftplayer_points]
             return current_anotatedscore + "-All" if (self.leftplayer_points == self.rightplayer_points) else current_anotatedscore + "-" + score_annotation[self.rightplayer_points]
         else:
             if self.leftplayer_points == self.rightplayer_points:
                 return "Deuce"
-            current_anotatedscore = self.leftplayer_name if self.leftplayer_points > self.rightplayer_points else self.rightplayer_name
+            winningplayer_name = self.leftplayer_name if self.leftplayer_points > self.rightplayer_points else self.rightplayer_name
             return (
-                "Advantage " + current_anotatedscore
+                "Advantage " + winningplayer_name
                 if ((self.leftplayer_points - self.rightplayer_points) * (self.leftplayer_points - self.rightplayer_points) == 1)
-                else "Win for " + current_anotatedscore
+                else "Win for " + winningplayer_name
             )
