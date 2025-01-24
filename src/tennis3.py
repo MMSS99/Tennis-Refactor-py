a40d0a070bd49f977f4321c04250d2ff846287c3
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-01-24 13:40:15 +0100

    refactor(tennis3): :recycle: rename all variables
    the variables had simple names that were not representative of the information they held

--- src/tennis3.py
+++ src/tennis3.py
@@ -1,27 +1,27 @@
 class TennisGame3:
     def __init__(self, leftplayer_name, rightplayer_name):
-        self.p1_n = leftplayer_name
-        self.p2_n = rightplayer_name
-        self.p1 = 0
-        self.p2 = 0
+        self.leftplayer_name = leftplayer_name
+        self.rightplayer_name = rightplayer_name
+        self.leftplayer_points = 0
+        self.rightplayer_points = 0
 
-    def won_point(self, n):
-        if n == "player1":
-            self.p1 += 1
+    def won_point(self, playername):
+        if playername == self.leftplayer_name:
+            self.leftplayer_points += 1
         else:
-            self.p2 += 1
+            self.rightplayer_points += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+        if (self.leftplayer_points < 4 and self.rightplayer_points < 4) and (self.leftplayer_points + self.rightplayer_points < 6):
+            score_annotation = ["Love", "Fifteen", "Thirty", "Forty"]
+            current_anotatedscore = score_annotation[self.leftplayer_points]
+            return current_anotatedscore + "-All" if (self.leftplayer_points == self.rightplayer_points) else current_anotatedscore + "-" + score_annotation[self.rightplayer_points]
         else:
-            if self.p1 == self.p2:
+            if self.leftplayer_points == self.rightplayer_points:
                 return "Deuce"
-            s = self.p1_n if self.p1 > self.p2 else self.p2_n
+            current_anotatedscore = self.leftplayer_name if self.leftplayer_points > self.rightplayer_points else self.rightplayer_name
             return (
-                "Advantage " + s
-                if ((self.p1 - self.p2) * (self.p1 - self.p2) == 1)
-                else "Win for " + s
+                "Advantage " + current_anotatedscore
+                if ((self.leftplayer_points - self.rightplayer_points) * (self.leftplayer_points - self.rightplayer_points) == 1)
+                else "Win for " + current_anotatedscore
             )
